720158d22b51bc43348eb2968e691ac4f0c7cb3d
Grace Reeder <gracereeder1@gmail.com>
2021-02-03 13:10:35 -0700

    added pygame

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import pygame, sys, random
+from pygame.locals import *
+pygame.init()
+win = pygame.display.set_mode((100, 100))
+
 class Pacman:
     def __init__(self, pos=(0, 0)):
         self.pos = pos
+        self.current_dir = ()
         self.next_dir = ()
-        if self.next_dir:
-            print("true")
+        self.num_pellets = 0
 
 p = Pacman()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
